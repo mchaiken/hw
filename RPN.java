4b7ce607d732680c4189a77347c7ad0f868fc934
mchaiken <mirandachaiken@aol.com>
2014-04-07 19:45:06 -0400

    RPN in progress
    (untested due to lack of java on this computer)

--- RPN.java
+++ RPN.java
@@ -1,1 +1,50 @@
-public class RPN{}
+import java.util.*;
+
+public class RPN{
+  private PancakeStack stack;
+  private static final String opperators="*/+-";
+  private boolean on? = false;
+  
+  
+  public void calculate(){
+    on? = true;
+    while (on?){
+      System.out.print("Type and hit Enter:");
+      Scanner sc = new Scanner(System.in);
+      String input=sc.nextLine();
+      if(input.equals("off")){
+        on?=false;
+      }
+      else if(opperators.indexOf(input) ==-1){
+          stack.push(index)
+      }
+      else{
+        System.out.println(opperate(input));
+      }
+    }
+  }
+  
+  public double opperate(String op){
+    double b=Double.parseDouble(stack.pop());
+    double a=Double.parseDouble(stack.pop());
+    double ret=0;
+    if(op.equals("*")){
+      ret=a*b;
+    }
+    else if(op.equals("/")){
+      ret=a/b;
+    }
+    
+    else if(op.equals("+")){
+      ret=a+b;
+    }
+    else if(op.equals("-")){
+      ret=a-b;
+    }
+    stack.push(ret)
+    return ret;
+  }
+
+}
+
+  
