714701c6ad4d4b2f37b30f38ea901ea4a1a52dab
mchaiken <mirandachaiken@gmail.com>
2014-04-27 18:48:50 -0400

    BTS

--- 16-Tree/BinaryTree.java
+++ 16-Tree/BinaryTree.java
@@ -1,17 +1,72 @@
 public class BinaryTree{
     private Node root;
-    private Node dummy;
 
     public BinaryTree(int x){
-	dummy=new Node(0);
 	root=new Node(x);
-	dummy.setRight(root);
-	dummy.setLeft(root);
+
     }
 
-    public int insert(int x){
-	Node temp=dummy;
-	Node temp2=root;
-	while(temp2 != null){
-	    if (temp2.getData(
+    public void insert(int x){
+	Node temp=root;
+	Node temp2;
+	while(temp != null){
+	    temp2= temp;
+	    if (temp2.getData() > x){
+		temp=temp.getLeft();
+		if (temp==null)
+		    temp2.setLeft(new Node(x));
+	    }
+	    else if(temp2.getData()<x){
+		temp=temp.getRight();
+		if (temp==null)
+		    temp2.setRight(new Node(x));
+	    }
+	}
+    }
+
+    public Node search(int x){
+	Node temp=root;
+	while(temp != null && temp.getData() != x){
+	    if (temp.getData() > x){
+		temp=temp.getLeft();
+	    }
+	    else if(temp.getData()<x){
+		temp=temp.getRight();
+	    }
+	}
+	return temp;
+    }
+
+
+    public Node search (int x, Node c){
+	if (c== null || c.getData() == x)
+	    return c;
+	else if (c.getData() > x)
+	    return search(x, c.getLeft());
+	else{
+	    return search(x, c.getRight()):
+	}
+    }
+
+    public String toString(Node temp){
+	if (temp != null){
+	    return temp.getData() +" R of "+temp.getData()+" is " + toString(temp.getRight())+ "L of "+temp.getData()+ " is " + toString(temp.getLeft());
+	}
+	return "nothing ";
+    }
+    public String toString(){
+	return toString(root);
+    }
+
+
+
+    public static void main(String[] args){
+	BinaryTree me = new BinaryTree(100);
+	me.insert(150);
+	me.insert(50);
+	me.insert(10);
+	me.insert(75);
+	System.out.println(me);
+    }
+	
 }
